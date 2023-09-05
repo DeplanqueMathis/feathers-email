3067a27ce1fc7af204968f1b72795540e1824930
Mathis Deplanque <mathisdeplanque@air-de-mathis.home>
2023-09-05 22:55:52 +0200

    Send email

--- src/hooks/sendEmail.js
+++ src/hooks/sendEmail.js
@@ -3,18 +3,24 @@ import nodemailer from 'nodemailer';
 
 export const sendEmail = async (context) => {
 
-  const account = await nodemailer.createTestAccount(); // internet required
+  let emailConf = {};
 
-  const emailConf = {
-    host: account.smtp.host,
-    port: account.smtp.port,
-    secure: account.smtp.secure, // 487 only
-    requireTLS: true,
-    auth: {
-      user: account.user, // generated ethereal user
-      pass: account.pass // generated ethereal password
-    }
-  };
+  if(!context.app.get('smtp')){
+    const account = await nodemailer.createTestAccount(); // internet required
+
+    emailConf = {
+      host: account.smtp.host,
+      port: account.smtp.port,
+      secure: account.smtp.secure, // 487 only
+      requireTLS: true,
+      auth: {
+        user: account.user, // generated ethereal user
+        pass: account.pass // generated ethereal password
+      }
+    };
+  } else {
+    emailConf = context.app.get('smtp');
+  }
 
   // Use the service
   const email = {
@@ -22,7 +28,6 @@ export const sendEmail = async (context) => {
   };
 
   let transporter = nodemailer.createTransport(emailConf);
-  
   transporter.sendMail(email).then(info=>{
     console.log('Preview URL: ' + nodemailer.getTestMessageUrl(info));
 });
